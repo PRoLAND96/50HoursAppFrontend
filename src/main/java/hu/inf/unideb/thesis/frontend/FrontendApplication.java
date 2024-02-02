2a2587907b7cad0de0b5a404bab1694dd5f12414
Pető Roland <petoroli2001@gmail.com>
2024-02-02 14:48:16 +0100

    initial commit

---
+++ src/main/java/hu/inf/unideb/thesis/frontend/FrontendApplication.java
@@ -0,0 +1,13 @@
+package hu.inf.unideb.thesis.frontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrontendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FrontendApplication.class, args);
+    }
+
+}
