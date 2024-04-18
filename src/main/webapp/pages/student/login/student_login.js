23c61ee0e794392398c7a2e4237c4b07db944d32
Pető Roland <petoroli2001@gmail.com>
2024-04-18 14:22:35 +0200

    fixing login

--- src/main/webapp/pages/student/login/student_login.js
+++ src/main/webapp/pages/student/login/student_login.js
@@ -1,25 +1,39 @@
-function login() {
-    const username = document.getElementById('username').value;
-    const password = document.getElementById('password').value;
+document.addEventListener('DOMContentLoaded', function() {
+    const loginForm = document.getElementById('loginForm');
+    const loginMessage = document.getElementById('loginMessage');
 
-    fetch(apiURL + 'perform_login', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            username: username,
-            password: password
-        })
-    })
-    .then(response => {
-        if (!response.ok) {
-            //throw new Error('Hiba történt a kérés során.');
-        window.location.href = 'http://localhost:9090/50hours/pages/contact/home/contact_home.html';
-        }
-    })
-    .catch(error => {
-        console.error('Hiba:', error);
-        alert('Hiba történt a bejelentkezés során.');
+    loginForm.addEventListener('submit', function(event) {
+        event.preventDefault(); // Megakadályozza az űrlap alapértelmezett beküldését
+
+        const username = document.getElementById('username').value;
+        const password = document.getElementById('password').value;
+
+        // Ellenőrizd a felhasználói adatokat
+        loginUser(username, password);
     });
+});
+
+function loginUser(username, password) {
+    fetch(apiURL + 'users')
+        .then(response => response.json())
+        .then(data => {
+            const user = data.find(user => user.username === username && user.password === password);
+            if (user) {
+                // Sikeres bejelentkezés
+                document.getElementById('loginMessage').textContent = 'Sikeres bejelentkezés!';
+                
+                // Beállítjuk a currentUserID-t a sikeresen bejelentkezett user id-jére
+                let currentUserID = user.id;
+
+                // Átirányítás a home oldalra
+                window.location.href = '../home/student_home.html';
+            } else {
+                // Sikertelen bejelentkezés
+                document.getElementById('loginMessage').textContent = 'Hibás felhasználónév vagy jelszó!';
+            }
+        })
+        .catch(error => {
+            console.error('Error fetching user data:', error);
+            document.getElementById('loginMessage').textContent = 'Hiba történt a bejelentkezés során.';
+        });
 }
